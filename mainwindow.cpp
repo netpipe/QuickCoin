6e22e6dfc5f0815e461238572c21911bef77c8b8
Clay Shippy <teslaenergy@gmail.com>
2024-12-10 21:55:17 -0700

    gui update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 #include <QJsonDocument>
@@ -17,8 +17,8 @@ MainWindow::~MainWindow() {
 
 void MainWindow::fetchPrice(const QString &cryptoId, const QString &currency) {
 
-    QString cryptoId2 = ui->coin->currentText().toLatin1();
-QString currency2 = ui->country->currentText().toLatin1();
+    QString cryptoId2 = ui->cointxt->text().toLatin1();
+QString currency2 = ui->countrytxt->text().toLatin1();
     QString url = QString("https://api.coingecko.com/api/v3/simple/price?ids=%1&vs_currencies=%2")
                       .arg(cryptoId2, currency2);
     qDebug() << "Requesting URL:" << url;
@@ -47,7 +47,7 @@ void MainWindow::handleNetworkReply() {
                     qDebug() << "Price of" << key << "in" << currency << ":" << price;
 
                     // Update the newprice field in the UI
-                    ui->newprice->setText(QString::number(price, 'f', 15 ));
+                    ui->newprice->setText(QString::number(price, 'f', 10 ));
 
                     // Recalculate values based on the new price
                     calculateFinalValues();
@@ -90,5 +90,18 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::on_pushButton_2_clicked()
 {
     calculateFinalValues();
+}
+
+
+void MainWindow::on_coin_activated(const QString &arg1)
+{
+    ui->cointxt->setText( ui->coin->currentText().toLatin1());
+//QString currency2
+}
+
+
+void MainWindow::on_country_activated(const QString &arg1)
+{
+     ui->countrytxt->setText( ui->country->currentText().toLatin1());
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -25,6 +25,10 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_coin_activated(const QString &arg1);
+
+    void on_country_activated(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
     QNetworkAccessManager *manager;
